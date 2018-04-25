fddf4c5a29c7bb04e4543410daab7eb41ecdb51e
Md. Abu Said Sarkar <said@akij.net>
2018-04-25 12:05:58 +0600

    SalesPrice

--- UI/AEFPS/SalesPriceChange.aspx.cs
+++ UI/AEFPS/SalesPriceChange.aspx.cs
@@ -20,19 +20,18 @@ namespace UI.AEFPS
     {
         Receive_BLL objRec = new Receive_BLL();
         DataTable dt = new DataTable();
-        int enroll, mrrId, intWh, rack = 1, godown = 2, rackType; string ImagePath = "", rackId = "0", receiveQty;
+        int enroll, mrrId, intWh; string  receiveQty;
 
       
 
         string filePathForXML; string xmlString = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-            filePathForXML = Server.MapPath("~/AEFPS/Data/Reca__" + HttpContext.Current.Session[SessionParams.USER_ID].ToString() + ".xml");
+            
 
             if (!IsPostBack)
             {
-                try { File.Delete(filePathForXML); dgvReceive.DataSource = ""; dgvReceive.DataBind(); }
-                catch { }
+                
                 DefaltLoad();
                 pnlUpperControl.DataBind();
             }
@@ -56,14 +55,14 @@ namespace UI.AEFPS
                 ddlWH.DataBind();
 
                 intWh = int.Parse(ddlWH.SelectedValue.ToString());
-                dt = objRec.DataView(2, "", intWh, 0, DateTime.Now, enroll);
+                dt = objRec.DataView(17, "", intWh, 0, DateTime.Now, enroll);
                 ddlMrrNo.DataSource = dt;
                 ddlMrrNo.DataTextField = "strName";
                 ddlMrrNo.DataValueField = "Id";
                 ddlMrrNo.DataBind();
 
                 mrrId = int.Parse(ddlMrrNo.SelectedValue.ToString());
-                dt = objRec.DataView(17, "", intWh, mrrId, DateTime.Now, enroll);
+                dt = objRec.DataView(18, "", intWh, mrrId, DateTime.Now, enroll);
                 dgvReceive.DataSource = dt;
                 dgvReceive.DataBind();
 
@@ -79,7 +78,7 @@ namespace UI.AEFPS
                 enroll = int.Parse(HttpContext.Current.Session[SessionParams.USER_ID].ToString());
                 mrrId = int.Parse(ddlMrrNo.SelectedValue.ToString());
                 intWh = int.Parse(ddlWH.SelectedValue.ToString());
-                dt = objRec.DataView(3, "", intWh, mrrId, DateTime.Now, enroll);
+                dt = objRec.DataView(18, "", intWh, mrrId, DateTime.Now, enroll);
                 dgvReceive.DataSource = dt;
                 dgvReceive.DataBind();
             }
@@ -91,59 +90,56 @@ namespace UI.AEFPS
             {
                 enroll = int.Parse(HttpContext.Current.Session[SessionParams.USER_ID].ToString());
                 intWh = int.Parse(ddlWH.SelectedValue.ToString());
-                dt = objRec.DataView(2, "", intWh, 0, DateTime.Now, enroll);
+                dt = objRec.DataView(17, "", intWh, 0, DateTime.Now, enroll);
                 ddlMrrNo.DataSource = dt;
                 ddlMrrNo.DataTextField = "strName";
                 ddlMrrNo.DataValueField = "Id";
                 ddlMrrNo.DataBind();
 
                 mrrId = int.Parse(ddlMrrNo.SelectedValue.ToString());
-                dt = objRec.DataView(3, "", intWh, mrrId, DateTime.Now, enroll);
+                dt = objRec.DataView(18, "", intWh, mrrId, DateTime.Now, enroll);
                 dgvReceive.DataSource = dt;
                 dgvReceive.DataBind();
             }
             catch { }
         }
 
-        protected void btnPrint_Click(object sender, EventArgs e)
-        {
-           
-
-        }
+      
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
             try
-            {
-                
+            { 
                 if (int.Parse(hdnConfirm.Value) > 0)
                 {
-
                     enroll = int.Parse(HttpContext.Current.Session[SessionParams.USER_ID].ToString());
                     intWh = int.Parse(ddlWH.SelectedValue.ToString());
                     mrrId = int.Parse(ddlMrrNo.SelectedValue.ToString());
 
                     GridViewRow row = (GridViewRow)((Button)sender).NamingContainer;
-                    TextBox txtSalesQty = row.FindControl("txtReceQty") as TextBox;
+                     
                     TextBox txtSalesPrice = row.FindControl("txtSalesPrice") as TextBox;
-                    TextBox txtPrintQty = row.FindControl("txtPrtQty") as TextBox;
+                    
                     TextBox txtDteDate = row.FindControl("txtExpireDate") as TextBox;
                     Label lblItemMaster = row.FindControl("lblItemMaster") as Label;
-                    Label lblRemainQty = row.FindControl("lblRemaingQty") as Label;
+                    Label lblAutoID = row.FindControl("lblAutoID") as Label;
+                    Label lblStockQty = row.FindControl("lblStockQty") as Label;
+
                     Label lblItem = row.FindControl("lblItemId") as Label;
                     Label lblItemName = row.FindControl("lblItemName") as Label;
                     string itemid = lblItem.Text.ToString();
                     string ItemName = lblItemName.Text.ToString();
-                    string itmMaster = lblItemMaster.Text.ToString();
-                    string ReceiveQty = txtSalesQty.Text.ToString();
+                    string itmMaster = lblItemMaster.Text.ToString(); 
                     string salseprice = txtSalesPrice.Text.ToString();
                     string dteDate = txtDteDate.Text.ToString();
-                    decimal reminQty = decimal.Parse(lblRemainQty.Text.ToString());
-                    string xmlunit = "<voucher><voucherentry itemId=" + '"' + itemid + '"' + " ItmMaster=" + '"' + itmMaster + '"' + " SalesPrice=" + '"' + salseprice + '"' + " ReceiveQty=" + '"' + ReceiveQty + '"' + " rackId=" + '"' + rackId + '"' + " RackType=" + '"' + rackType.ToString() + '"' + " dteDate=" + '"' + dteDate + '"' + "/></voucher>".ToString();
+                    string strAutoID = lblAutoID.Text.ToString();
+                    decimal stockQty = decimal.Parse(lblStockQty.Text.ToString());
+
+                    string xmlunit = "<voucher><voucherentry itemId=" + '"' + itemid + '"' + " ItmMaster=" + '"' + itmMaster + '"' + " SalesPrice=" + '"' + salseprice + '"' + " strAutoID=" + '"' + strAutoID + '"'  + " dteDate=" + '"' + dteDate + '"' + "/></voucher>".ToString();
                      
-                    if (decimal.Parse(salseprice) > 0)
+                    if (decimal.Parse(salseprice) > 0 && stockQty>0)
                     {
-                        string mrtg = objRec.MrrReceiveInsert(4, xmlunit, intWh, mrrId, DateTime.Now, enroll);
+                        string mrtg = objRec.MrrReceiveInsert(19, xmlunit, intWh, mrrId, DateTime.Now, enroll);
                         ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "alert('" + mrtg + "');", true);
 
                     }
@@ -152,16 +148,12 @@ namespace UI.AEFPS
                         ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "alert('Receive Quantity Grater then Mrr Quantity);", true);
                     }
 
-                    dt = objRec.DataView(3, "", intWh, mrrId, DateTime.Now, enroll);
+                    dt = objRec.DataView(18, "", intWh, mrrId, DateTime.Now, enroll);
                     dgvReceive.DataSource = dt;
                     dgvReceive.DataBind();
 
                 }
-                else
-                {
-                    ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "alert('Please select Rack Type');", true);
-
-                }
+                
             }
             catch { }
         }
