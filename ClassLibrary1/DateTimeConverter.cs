165d54918939c0a0bfdbfb677841847b49fc5abd
arafat.corp <arafat.corp@AGLAPTOP-191>
2018-04-29 19:17:14 +0600

    Add new Project Utility

---
+++ ClassLibrary1/DateTimeConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class DateTimeConverter
+    {
+        public static DateTime StringToDateTime(string date, string format)
+        {
+            return DateTime.ParseExact(date, "yyyy-MMMM", System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+}
