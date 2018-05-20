1d40f581c982d5c591b57bcf72cac6171d7495ba
arafat.corp <arafat.corp@AGLAPTOP-191>
2018-05-20 16:35:46 +0600

    added 2 model

---
+++ Model/ModularOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ModularOrder
+    {
+        public string OrderNumber { get; set; }
+        public string OrderDescription { get; set; }
+        public string OperationType { get; set; }
+    }
+}
---
+++ Model/ModularOrderLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ModularOrderLine
+    {
+        public string OrderNumber { get; set; }
+        public string ItemCode { get; set; }
+        public string Quantity { get; set; }
+    }
+}
