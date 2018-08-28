0b388114d3508764fe6dc36519ccbadc890db35e
monirul <monirul@AGLAPTOP-1237>
2018-08-28 13:55:27 +0600

    do afml

--- UI/LoginProcess.aspx.cs
+++ UI/LoginProcess.aspx.cs
@@ -56,7 +56,7 @@ namespace UI
             {
                 string[] donainpatrs = domainUser.Split('\\');
                 id = donainpatrs[1] + "@akij.net";
-               // id = "test.ajml@akij.net";
+                //id = "shariful.afml@akij.net";
 
                 string ip = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
                 if (ip == string.Empty || ip == null)
