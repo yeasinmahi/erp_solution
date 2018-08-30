96727c613144683bd5093d8113ff4a5dfe58f753
binti.corp <binti.corp@AGLAPTOP-1319>
2018-08-30 09:20:56 +0600

    Customer Bank Guarantee Report and Customer Transaction

---
+++ UI/SAD/Sales/Report/RptRemoteSales/DOBaseCustomerTransaction.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.SAD.Sales.Report.RptRemoteSales
+{
+    public partial class DOBaseCustomerTransaction :BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'https://report.akij.net/reports/report/Ruhul/Binti/CustomerTransactionReport?rs:Embed=true');", true);
+
+        }
+    }
+}
