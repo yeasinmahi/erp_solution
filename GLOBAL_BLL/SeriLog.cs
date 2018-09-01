d095e2fa20448c2192c47122004e66991d59fd0d
said <said@akij.net>
2018-09-01 12:27:26 +0600

    checj

---
+++ GLOBAL_BLL/SeriLog.cs
@@ -0,0 +1,26 @@
+﻿using Flogging.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GLOBAL_BLL
+{
+    public class SeriLog
+    {
+        public FlogDetail GetFlogDetail(string message,string location,string Layer, Exception ex)
+        {
+            return new FlogDetail
+            {
+                Product = "ERP",
+                Location = location,
+                Layer = Layer,
+                UserName = Environment.UserName,
+                Hostname = Environment.MachineName,
+                Message = message+"\\"+ Layer,
+                Exception = ex
+            };
+        }
+    }
+}
