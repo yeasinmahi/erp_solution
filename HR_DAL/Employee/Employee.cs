85460dc9b84129b6e058cc4fc88357e076922e6a
binti.corp <binti.corp@akij.net>
2018-10-27 09:39:55 +0600

    benifitEntry

---
+++ HR_DAL/Employee/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace HR_DAL.Employee
+{
+
+
+    partial class Employee
+    {
+    }
+}
+
+namespace HR_DAL.Employee.EmployeeTableAdapters {
+    
+    
+    public partial class QryEmployeeProfileAllTableAdapter {
+    }
+}
