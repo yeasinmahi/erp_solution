ca90cde82956b3372c46bd1fa467deb7bcc3a937
arafat.corp <arafat.corp@akij.net>
2018-10-28 18:23:17 +0600

    design ImportFileManagement page

---
+++ UI/Import/ImportFileManagement.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UI.Import
+{
+    public partial class ImportFileManagement : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            pnlUpperControl.DataBind();
+            if (!IsPostBack)
+            {
+                
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
