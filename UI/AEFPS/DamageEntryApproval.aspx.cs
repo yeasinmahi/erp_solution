792e8e1a418404c5b2391eb235dc87e9e5e1d89a
binti.corp <binti.corp@akij.net>
2018-11-12 15:36:54 +0600

    damage

---
+++ UI/AEFPS/DamageEntryApproval.aspx.cs
@@ -0,0 +1,42 @@
+﻿using SAD_BLL.AEFPS;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UI.AEFPS
+{
+    public partial class DamageEntryApproval : System.Web.UI.Page
+    {
+        readonly Receive_BLL _bll = new Receive_BLL();
+
+        int _intEnroll = 373605; //------------=========------------------ VULE GELE HOBENA---------------==========------------//
+
+        DataTable dt = new DataTable();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //_intEnroll = int.Parse(HttpContext.Current.Session[SessionParams.USER_ID].ToString());
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+                LoadWh();
+            }
+        }
+        private void LoadWh()
+        {
+            ddlWh.DataSource = _bll.DataView(1, "", 0, 0, DateTime.Now, _intEnroll);
+            ddlWh.DataTextField = "strName";
+            ddlWh.DataValueField = "Id";
+            ddlWh.DataBind();
+        }
+
+
+
+
+
+
+    }
+}
