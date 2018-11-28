a0da8138cd385b1b7d9c23709a1d023ead54050a
binti.corp <binti.corp@akij.net>
2018-11-28 15:21:49 +0600

    Procurement report

---
+++ UI/SCM/ProcurementStatus.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.SCM
+{
+    public partial class ProcurementStatus : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            { 
+                pnlUpperControl.DataBind(); 
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'https://report.akij.net/reports/report/SCM/Procurement_Status?rs:Embed=true');", true);
+
+        }
+    }
+}
