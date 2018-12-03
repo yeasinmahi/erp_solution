ac5597cbe5a3e14889cea2c2d486b3e372889303
nafees.corp <nafees.corp@akij.net>
2018-12-03 16:57:42 +0600

    Product Loading status

---
+++ UI/SAD/Sales/Report/ProductLoadingStatus.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.SAD.Sales.Report
+{
+    public partial class ProductLoadingStatus : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'http://report.akij.net/reports/report/Sales%20And%20Distribution/Product_Loading_Status?rs:Embed=true');", true);
+        }
+    }
+}
