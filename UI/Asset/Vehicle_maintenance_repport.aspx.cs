ee7de947de00735d416ae32ae2599a5866ef792b
nafees.corp <nafees.corp@akij.net>
2018-12-10 10:07:20 +0600

    Asset Module reports

---
+++ UI/Asset/Vehicle_maintenance_repport.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.Asset
+{
+    public partial class Vehicle_maintenance_repport : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'https://report.akij.net/reports/report/Asset_Module/Vehicle_Maintenance_Report?rs:Embed=true');", true);
+        }
+    }
+}
