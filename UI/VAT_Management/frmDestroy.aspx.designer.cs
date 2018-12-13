5ce09295f4ee39e8cccb37076b8738254d1a67b4
ikbal <ikbal@AGCORP-WS16>
2018-12-13 11:34:05 +0600

    Vat

--- UI/VAT_Management/frmDestroy.aspx.designer.cs
+++ UI/VAT_Management/frmDestroy.aspx.designer.cs
@@ -220,6 +220,15 @@ namespace UI.VAT_Management {
         protected global::System.Web.UI.WebControls.Label lblHeading;
         
         /// <summary>
+        /// ddlMushoktype control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlMushoktype;
+        
+        /// <summary>
         /// txtVatItemList control.
         /// </summary>
         /// <remarks>
