1f5cc9f4d3d9b619f64bec0f9f2ce1912ff178fb
binti.corp <binti.corp@akij.net>
2018-12-22 10:42:08 +0600

    Inventory Statement

---
+++ UI/Inventory/Inventory_Statement_Report.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.Inventory
+{
+    public partial class Inventory_Statement_Report : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'https://report.akij.net/Reports/report/Corporate/Inventory_Report_New?rs:Embed=true');", true);
+
+        }
+    }
+}
