a751e0e5909c9f9e20c59da1b41b9aad4e9b08bf
arafat.corp <arafat.corp@akij.net>
2019-01-05 11:21:52 +0600

    Inventory statement wh load changed

--- UI/Inventory/Inventory_Statement_Report.aspx.cs
+++ UI/Inventory/Inventory_Statement_Report.aspx.cs
@@ -8,8 +8,9 @@ namespace UI.Inventory
 {
     public partial class Inventory_Statement_Report : BasePage
     {
-        DataTable dt = new DataTable();
-        InventoryTransfer_BLL objbll = new InventoryTransfer_BLL();
+        private DataTable dt = new DataTable();
+        private InventoryTransfer_BLL objbll = new InventoryTransfer_BLL();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -18,10 +19,10 @@ namespace UI.Inventory
                 try
                 {
                     int enroll = Convert.ToInt32(Session[SessionParams.USER_ID].ToString());
-                    dt = objbll.GetWH(enroll);
+                    dt = new MrrReceive_BLL().DataView(19, "", 0, 0, DateTime.Now, enroll);
                     ddlWH.DataSource = dt;
-                    ddlWH.DataTextField = "strWareHoseName";
-                    ddlWH.DataValueField = "intWHID";
+                    ddlWH.DataTextField = "strName";
+                    ddlWH.DataValueField = "Id";
                     ddlWH.DataBind();
                     DateTime now = DateTime.Now;
                     var dte = new DateTime(now.Year, now.Month, 1);
@@ -30,8 +31,6 @@ namespace UI.Inventory
                 }
                 catch { }
             }
-
-            
         }
 
         protected void ddlSearchBy_SelectedIndexChanged(object sender, EventArgs e)
@@ -40,10 +39,9 @@ namespace UI.Inventory
             int Type = Convert.ToInt32(ddlSearchBy.SelectedItem.Value);
             int WH = Convert.ToInt32(ddlWH.SelectedItem.Value);
             try { itemId = Convert.ToInt32(txtItemID.Text); }
-            catch {  itemId = 0; }
+            catch { itemId = 0; }
 
             ScriptManager.RegisterClientScriptBlock(Page, typeof(Page), "Script", "showPanel();", true);
-         
 
             dt = objbll.InventorySearch(Type, WH, itemId);
             ddlSubCategory.DataSource = dt;
@@ -54,19 +52,18 @@ namespace UI.Inventory
 
         protected void btnShow_Click(object sender, EventArgs e)
         {
-
-            string id="";
+            string id = "";
             int WH = Convert.ToInt32(ddlWH.SelectedItem.Value);
             int ddlsearch = Convert.ToInt32(ddlSearchBy.SelectedItem.Value);
-            if(ddlsearch==4)
+            if (ddlsearch == 4)
             {
                 id = txtItemName.Text;
                 txtItemID.Text = "";
                 try
                 {
-                    if(!string.IsNullOrEmpty(ddlSubCategory.SelectedItem.Text))
+                    if (!string.IsNullOrEmpty(ddlSubCategory.SelectedItem.Text))
                     {
-                        ddlSubCategory.DataSource=null;
+                        ddlSubCategory.DataSource = null;
                         ddlSubCategory.DataBind();
                     }
                 }
@@ -76,7 +73,7 @@ namespace UI.Inventory
                     ddlSubCategory.DataBind();
                 }
             }
-            else if(ddlsearch==3)
+            else if (ddlsearch == 3)
             {
                 id = txtItemID.Text;
             }
@@ -85,7 +82,7 @@ namespace UI.Inventory
                 id = "";
                 ddlsearch = 4;
             }
-            else if(ddlsearch==2)
+            else if (ddlsearch == 2)
             {
                 id = ddlSubCategory.SelectedItem.Text;
             }
@@ -104,14 +101,13 @@ namespace UI.Inventory
             }
             else
             {
-                url = "https://report.akij.net/ReportServer/Pages/ReportViewer.aspx?/Open_Reports/Inventory_Report_New" + "&wh=" + ddlWH.SelectedItem.Value + "&SearchBy=" + ddlsearch + "&FromDate=" + txtFromDate.Text+" "+ fromTime + "&ToDate=" + txtToDate.Text + " " + toTime + "&strID=" + id + "&rc:LinkTarget=_self";
+                url = "https://report.akij.net/ReportServer/Pages/ReportViewer.aspx?/Open_Reports/Inventory_Report_New" + "&wh=" + ddlWH.SelectedItem.Value + "&SearchBy=" + ddlsearch + "&FromDate=" + txtFromDate.Text + " " + fromTime + "&ToDate=" + txtToDate.Text + " " + toTime + "&strID=" + id + "&rc:LinkTarget=_self";
             }
             ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', '" + url + "');", true);
-
         }
 
         protected void ddlWH_SelectedIndexChanged(object sender, EventArgs e)
-        {          
+        {
             ddlSearchBy.SelectedIndex = ddlSearchBy.Items.IndexOf(ddlSearchBy.Items.FindByValue("11"));
         }
     }
