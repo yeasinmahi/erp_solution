569dd1e2356397da08d4993c6606d95da6fa0c11
binti.corp <binti.corp@akij.net>
2019-01-07 14:20:34 +0600

    salary

---
+++ UI/AEFPS/Total_Salary_Adjustment_Report.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.AEFPS
+{
+    public partial class Total_Salary_Adjustment_Report : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'https://report.akij.net/reports/report/AEFPS/Total%20SalaryAdjustment%20Report?rs:Embed=true');", true);
+
+        }
+    }
+}
