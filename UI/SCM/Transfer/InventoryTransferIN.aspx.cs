448c67464a2795eb52b83ecbff8ada1b90ddbf15
arafat.corp <arafat.corp@akij.net>
2019-01-31 15:19:26 +0600

    nothing

--- UI/SCM/Transfer/InventoryTransferIN.aspx.cs
+++ UI/SCM/Transfer/InventoryTransferIN.aspx.cs
@@ -5,8 +5,6 @@ using System.Data;
 using System.Web;
 using System.Web.Script.Services;
 using System.Web.Services;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using UI.ClassFiles;
 using Utility;
 
