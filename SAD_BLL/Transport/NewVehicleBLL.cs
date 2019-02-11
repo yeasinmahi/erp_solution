197cc48f096a1ffd835818b146d02c906d7f5bdc
ikbal <ikbal@AGCORP-WS16>
2019-02-11 09:32:22 +0600

    Vehicle entry

---
+++ SAD_BLL/Transport/NewVehicleBLL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SAD_DAL.Transport;
+using SAD_DAL.Transport.NewVehicleRegTDSTableAdapters;
+
+namespace SAD_BLL.Transport
+{
+    public class NewVehicleBLL
+    {
+
+        private static NewVehicleRegTDS.TblVehicleTypeDataTable[] tableProducts = null;
+        private static Hashtable ht = new Hashtable();
+
+        public void getVehicle(NewVehicleRegTDS.TblVehicleTypeRow tw,string text)
+        {
+            
+            tw.intUnitId = 2;
+
+
+        }
+    }
+}
