f38c3d8de24a98e2562d528f09c7442e7e6dec56
binti.corp <binti.corp@akij.net>
2019-03-05 12:25:15 +0600

    Employee Previous History

--- UI/HR/Reports/Employee_Previous_History.aspx.designer.cs
+++ UI/HR/Reports/Employee_Previous_History.aspx.designer.cs
@@ -85,13 +85,13 @@ namespace UI.HR.Reports {
         protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
-        /// txtEmpSearch control.
+        /// txtEmployeeSearch control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtEmpSearch;
+        protected global::System.Web.UI.WebControls.TextBox txtEmployeeSearch;
         
         /// <summary>
         /// Label2 control.
@@ -119,5 +119,14 @@ namespace UI.HR.Reports {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnShow;
+        
+        /// <summary>
+        /// frame control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlIframe frame;
     }
 }
