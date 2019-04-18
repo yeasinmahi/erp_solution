893a16a034ae2f1704b6cd3d96815974ad24bafb
binti.corp <binti.corp@akij.net>
2019-04-18 10:07:19 +0600

    Benifit Entry

--- UI/HR/Benifit/BenifitsEntry.aspx.designer.cs
+++ UI/HR/Benifit/BenifitsEntry.aspx.designer.cs
@@ -94,15 +94,6 @@ namespace UI.HR.Benifit {
         protected global::System.Web.UI.WebControls.HiddenField hdnConfirm;
         
         /// <summary>
-        /// lblD control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblD;
-        
-        /// <summary>
         /// btnDownload control.
         /// </summary>
         /// <remarks>
