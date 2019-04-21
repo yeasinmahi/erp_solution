322953e23383422036344608f403865a66469acf
ikbal <ikbal@AGCORP-WS16>
2019-04-21 11:40:12 +0600

    test

--- SAD_BLL/Global/ShipPointOOP.cs
+++ SAD_BLL/Global/ShipPointOOP.cs
@@ -5,11 +5,14 @@ using System.Text;
 using SAD_DAL.Global.ShipPointTDSTableAdapters;
 using SAD_DAL.Global;
 using System.Data;
+using SAD_DAL.Global.ShipingPointOOPTableAdapters;
 
 namespace SAD_BLL.Global
 {
-    public class ShipPoint
+    public class ShipPointOOP
     {
+        DataTable dtTemp = new DataTable();
+        DataTable dtTemp2 = new DataTable();
         public ShipPointTDS.TblShippingPointBySODataTable GetSODataById(string shipPointId)
         {
             TblShippingPointBySOTableAdapter ta = new TblShippingPointBySOTableAdapter();
@@ -26,14 +29,45 @@ namespace SAD_BLL.Global
         }
         public DataTable GetShipingPoint(int userID,int Unitid)
         {
+            DataTable dt = new DataTable();
+            dt = GetShipPoint(userID, Unitid);
+            if (dt.Rows.Count > 0)
+            {
+              int  counts =int.Parse(dt.Rows[0]["intLevel"].ToString());
+               
+                if (counts <= 0)
+                {
+                    dt = getShip(Unitid);
+                    dtTemp = (from DataRow dr in dt.Rows  select dr).CopyToDataTable();                  
+                    dtTemp.Merge(dtTemp);
 
+                    return dtTemp;
+                }
+                else
+                {
+                    dt = getShip(Unitid);
+                    dtTemp = (from DataRow dr in dt.Rows select dr).CopyToDataTable();
+                    dtTemp.Merge(dtTemp);
 
-
-            return new DataTable();
+                    return dtTemp;
+                }
+            }
+                    return new DataTable();
         }
 
+        private DataTable getShip(int Unitid)
+        {
+            tblShippingPointTableAdapter adp = new tblShippingPointTableAdapter();
+            return adp.GetData(Unitid);
+        }
 
-            public string GetPrefix(string shipPoint)
+        private DataTable GetShipPoint(int userID,int Unitid)
+        {
+            qryShipPointByOperatorTableAdapter adp = new qryShipPointByOperatorTableAdapter();
+            return adp.GetShipPoint(userID, Unitid);
+        }
+
+        public string GetPrefix(string shipPoint)
         {
             if (shipPoint == "") return "";
             TblShippingPointBySOTableAdapter ta = new TblShippingPointBySOTableAdapter();
