96e220dcd9d48f6793d0496a81310e5ba34ad73a
arafat.corp <arafat.corp@akij.net>
2019-04-30 15:44:11 +0600

    Added sms class to project

---
+++ BLL/AFBLSMSServer/APIsmsBLL.cs
@@ -0,0 +1,35 @@
+﻿using DALOOP.AFBLSMSServer;
+using Model;
+using System;
+#pragma warning disable 168
+
+namespace BLL.AFBLSMSServer
+{
+    public class ApiSmsBll
+    {
+        #region INIT
+
+        private readonly ApiSmsDal _apiSmsDal = new ApiSmsDal();
+        #endregion
+
+        public bool InsertApiSms(ApiSmsModel model)
+        {
+            bool result;
+            try
+            {
+                if (model.InsertDate == DateTime.MinValue || model.InsertDate == null)
+                {
+                    model.InsertDate = DateTime.Now;
+                }
+
+                result = _apiSmsDal.InsertApIsms(model);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Model/APIsmsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Model
+{
+    public class ApiSmsModel
+    {
+        public int UnitId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string MaskingClient { get; set; }
+        public string PhoneNo { get; set; }
+        public string Message { get; set; }
+        public string Sms { get; set; }
+        public DateTime InsertDate { get; set; }
+    }
+}
