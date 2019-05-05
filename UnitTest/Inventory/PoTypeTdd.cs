10036f0040c9a1e4a84623257fe2545e449d81f5
arafat.corp <arafat.corp@akij.net>
2019-05-05 10:45:40 +0600

    Added folder system in Unit test

---
+++ UnitTest/Inventory/PoTypeTdd.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using BLL.Inventory;
+using NUnit.Framework;
+
+namespace UnitTest.Inventory
+{
+    [TestFixture]
+    public class PoTypeTdd
+    {
+        private readonly PoTypeBll _dal = new PoTypeBll();
+        [Test]
+        public void Bll_GetAllPoType_DataTable()
+        {
+            DataTable dt = _dal.GetAllPoType();
+            Assert.That(dt.Rows.Count > 0);
+        }
+    }
+}
