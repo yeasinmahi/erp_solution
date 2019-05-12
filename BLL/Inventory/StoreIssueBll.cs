4ff8162b6fd814b064e2cb9788cbe3a9741f54d9
arafat.corp <arafat.corp@akij.net>
2019-05-12 16:58:20 +0600

    Added some criteria for store issue

---
+++ BLL/Inventory/StoreIssueBll.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BLL.Inventory
+{
+    public class StoreIssueBll
+    {
+        
+        public void  StoreIssue(int whId, int itemId, decimal itemQuantity, decimal itemValue, int locationId,int enroll)
+        {
+            InventoryBll inventoryBll = new InventoryBll();
+            int jvId = inventoryBll.GetInventoryJvByDateType(DateTime.Now, 3);
+            if (jvId > 0)
+            {
+                ItemListBll itemList = new ItemListBll();
+                if (itemList.GetItemCoaId(itemId) > 0)
+                {
+                    //TODO: insert
+                }
+                else
+                {
+                    InconsistanceItemBll inconsistanceItemBll = new InconsistanceItemBll();
+                    if (inconsistanceItemBll.Insert(whId, itemId, itemQuantity, itemValue, locationId, enroll) > 0)
+                    {
+                        //TODO: success insert
+                    }
+                    else
+                    {
+                        //TODO: fails insert
+                    }
+                }
+                
+            }
+            else
+            {
+                //TODO: insert
+            }
+
+
+        }
+    }
+}
---
+++ DALOOP/Inventory/InventoryDal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using DAL.Inventory.InventoryTableAdapters;
+
+namespace DALOOP.Inventory
+{
+    public class InventoryDal
+    {
+        public DataTable GetInventoryJvByDateType(DateTime transectionDate, int transectionTypeId)
+        {
+            try
+            {
+                tblInventoryTableAdapter adp = new tblInventoryTableAdapter();
+                return adp.GetData(transectionDate, transectionTypeId);
+            }
+            catch (Exception e)
+            {
+                return new DataTable();
+            }
+        }
+    }
+}
