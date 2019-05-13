7c9d66a60093e8e586aef98473dae576ab3c701d
arafat.corp <arafat.corp@akij.net>
2019-05-13 10:51:01 +0600

    Added journal voucher BLL

---
+++ BLL/Accounts/AccountsVoucherJournalDetailsBll.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using DALOOP.Accounts;
+
+namespace BLL.Accounts
+{
+    public class AccountsVoucherJournalDetailsBll
+    {
+        private readonly AccountsVoucherJournalDetailsDal _dal = new AccountsVoucherJournalDetailsDal();
+        public int Insert(int intJournalVoucherId, int intAccId, string strNarration, decimal monAmount, string strAccName)
+        {
+            return _dal.Insert(intJournalVoucherId, intAccId, strNarration, monAmount, strAccName);
+        }
+
+        public DataTable GetJurnalVoucher(int voucherId)
+        {
+            return _dal.GetJurnalVoucherDetails(voucherId);
+        }
+    }
+}
