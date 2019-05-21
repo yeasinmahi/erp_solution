ae7082581fd1f2a4dfe5b6aa5cbcf8176c9c3c39
arafat.corp <arafat.corp@akij.net>
2019-05-21 11:28:14 +0600

    added some test case

--- UnitTest/Inventory/StoreIssueTdd.cs
+++ UnitTest/Inventory/StoreIssueTdd.cs
@@ -7,11 +7,11 @@ namespace UnitTest.Inventory
     public class StoreIssueTdd
     {
         private readonly StoreIssueBll _bll = new StoreIssueBll();
-        [TestCase(13,294973,1,0.1,45671, 369116)]
-        public void StoreIssue(int whId,int itemId,decimal itemQuantity,decimal itemValue,int locationId,int enroll)
-        {
-            bool result = _bll.StoreIssue(whId,itemId,itemQuantity,itemValue,locationId,enroll);
-            Assert.That(result);
-        }
+        //[TestCase(13,294973,1,0.1,45671, 369116)]
+        //public void StoreIssue(int whId,int itemId,decimal itemQuantity,decimal itemValue,int locationId,int enroll)
+        //{
+        //    bool result = _bll.StoreIssue(whId,itemId,itemQuantity,itemValue,locationId,enroll);
+        //    Assert.That(result);
+        //}
     }
 }
