2c28d26f5e24e1b47484a37307ebd2f43d969f1e
binti.corp <binti.corp@akij.net>
2019-05-25 16:27:40 +0600

    Picking Summary report

---
+++ UI/SAD/Delivery/Picking_Summary_Report.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+using Utility;
+
+namespace UI.SAD.Delivery
+{
+    public partial class Picking_Summary_Report : BasePage
+    {
+        DataTable dt = new DataTable();
+        HR_BLL.Global.Unit unitObj = new HR_BLL.Global.Unit();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+                LoadUnit();
+            }
+        }
+
+        private void LoadUnit()
+        {
+            dt = unitObj.GetUnits(HttpContext.Current.Session[SessionParams.USER_ID].ToString());
+            ddlUnit.Loads(dt, "intUnitID", "strUnit");
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            string unit = ddlUnit.SelectedItem.Value;
+            string Date = txtDate.Text;
+            LoadReport(unit, Date);
+        }
+
+        private void LoadReport(string unit, string Date)
+        {
+            string url = "https://report.akij.net/ReportServer/Pages/ReportViewer.aspx?/Common_Reports/Sales_And_Distribution/Picking_Summary" + "&Enroll=" + Enroll+ "&intUnitID=" + unit + "&date=" + Date + "&rc:LinkTarget=_self";
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', '" + url + "');", true);
+        }
+
+
+
+
+
+
+
+    }
+}
