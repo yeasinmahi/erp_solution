de4df027226fcca05d026c8318645644364c594a
arafat.corp <arafat.corp@akij.net>
2019-06-01 12:44:27 +0600

    Store Issue problem solved

---
+++ BLL/Inventory/InventoryLocationAndOpeningBll.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using DALOOP.Inventory;
+
+namespace BLL.Inventory
+{
+    public class InventoryLocationAndOpeningBll
+    {
+        private readonly InventoryLocationAndOpeningDal _dal = new InventoryLocationAndOpeningDal();
+
+        public int Insert(int intItem, int unitId, int whId, int locationId, int enroll)
+        {
+            return _dal.Insert(intItem, unitId, whId, locationId, enroll);
+        }
+        public DataTable GetInventoryLocationOpening(int intItem, int unitId, int whId, int locationId, int enroll)
+        {
+            return _dal.GetInventoryLocationOpening(intItem, whId, locationId);
+        }
+    }
+}
---
+++ BLL/Inventory/RequisitionDetailBll.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using DALOOP.Inventory;
+
+namespace BLL.Inventory
+{
+    public class RequisitionDetailBll
+    {
+        private readonly RequisitionDetailDal _dal = new RequisitionDetailDal();
+        private DataTable _dt = new DataTable();
+
+        public bool UpdateIssueQuantity(decimal issueQty, int reqId, int itemId)
+        {
+            return _dal.UpdateIssueQuantity(issueQty, reqId, itemId); 
+        }
+    }
+}
