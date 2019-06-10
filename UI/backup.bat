73089ecf755a3c7adf1e337f709ce1d71c3ddbc7
arafat.corp <arafat.corp@akij.net>
2019-06-10 15:37:51 +0600

    backup file change rollback

--- UI/backup.bat
+++ UI/backup.bat
@@ -1,11 +1,11 @@
 @echo off
 :: variables
-set drive=C:\Backup
+set drive=E:\Backup
 set folder=%date:~10,4%-%date:~7,2%-%date:~4,2%_%TIME:~0,2%-%TIME:~3,2%-%TIME:~6,2%
 set backupcmd=xcopy /s /c /d /e /h /i /r /k /y
 
 echo ### Backing up directory...
-%backupcmd% "C:\ERP" "%drive%\%folder%"
+%backupcmd% "E:\ERP" "%drive%\%folder%"
 
 echo Backup Complete!
 exit
