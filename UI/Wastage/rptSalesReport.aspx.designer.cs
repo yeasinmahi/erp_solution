c4446f6938b6eb10fe22ceb249d02cce58fecae0
iftakhairul.afbl <iftakhairul.afbl@AGCORP-WS496>
2019-06-27 10:00:30 +0600

    Print Report

--- UI/Wastage/rptSalesReport.aspx.designer.cs
+++ UI/Wastage/rptSalesReport.aspx.designer.cs
@@ -229,6 +229,15 @@ namespace UI.Wastage {
         protected global::System.Web.UI.WebControls.RadioButton rdnRptDetails;
         
         /// <summary>
+        /// btnprint control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnprint;
+        
+        /// <summary>
         /// btnShow control.
         /// </summary>
         /// <remarks>
