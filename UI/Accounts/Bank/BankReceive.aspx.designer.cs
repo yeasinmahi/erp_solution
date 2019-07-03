c6c777b2461c4e0075ab13831687377a955f5739
binti.corp <binti.corp@akij.net>
2019-07-03 10:55:57 +0600

    Bank receive

--- UI/Accounts/Bank/BankReceive.aspx.designer.cs
+++ UI/Accounts/Bank/BankReceive.aspx.designer.cs
@@ -58,15 +58,6 @@ namespace UI.Accounts.Bank {
         protected global::System.Web.UI.UpdatePanel UpdatePanel0;
         
         /// <summary>
-        /// pnlUpperControl control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel pnlUpperControl;
-        
-        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
