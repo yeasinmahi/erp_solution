0a1292b760f0cd227f1c3ca3198b012a65b1af05
binti.corp <binti.corp@akij.net>
2019-07-23 14:52:30 +0600

    Budget Report

---
+++ UI/SAD/Sales/Report/BudgetForFG.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.SAD.Sales.Report
+{
+    public partial class BudgetForFG : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'http://report.akij.net/reports/report/Sales%20And%20Distribution/BudgetforFG?rs:Embed=true');", true);
+
+        }
+    }
+}
---
+++ UI/SAD/Sales/Report/BudgetForMaterial.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.SAD.Sales.Report
+{
+    public partial class BudgetForMaterial : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'http://report.akij.net/reports/report/Sales%20And%20Distribution/BudgetforMaterial?rs:Embed=true');", true);
+
+        }
+    }
+}
