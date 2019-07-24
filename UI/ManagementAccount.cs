2a992e29f50200a277d71896bfd87096a9c9cc99
arafat.corp <arafat.corp@akij.net>
2019-07-24 13:52:06 +0600

    added Diagram

---
+++ UI/ManagementAccount.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+using System.ComponentModel;
+
+namespace UI
+{
+    public class ManagementAccount
+    {
+        public int Id
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int Amount
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int TaxAmount
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int VoucherId
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+}
---
+++ gitPull.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+repos=( 
+  ${PWD}
+)
+
+echo ""
+echo "Getting latest for" ${#repos[@]} "repositories using pull --rebase"
+
+for repo in "${repos[@]}"
+do
+  echo ""
+  echo "****** Getting latest for" ${repo} "******"
+  cd "${repo}"
+  git pull --rebase
+  echo "******************************************"
+  read -t 15 -n 1
+done
