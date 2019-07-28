aa1dff565838eea27a99d3807deaa9db11ed3595
binti.corp <binti.corp@akij.net>
2019-07-28 09:40:44 +0600

    payment advice

--- DAL/Accounts/Advice/Advice.cs
+++ DAL/Accounts/Advice/Advice.cs
@@ -1,1 +1,41 @@
-﻿
+﻿namespace DAL.Accounts.Advice
+{
+}
+
+namespace DAL.Accounts.Advice
+{
+}
+
+namespace DAL.Accounts.Advice
+{
+}
+
+namespace DAL.Accounts.Advice
+{
+}
+
+namespace DAL.Accounts.Advice
+{
+}
+
+namespace DAL.Accounts.Advice
+{
+}
+
+namespace DAL.Accounts.Advice
+{
+}
+namespace DAL.Accounts.Advice
+{
+
+
+    public partial class Advice
+    {
+    }
+}
+namespace DAL.Accounts.Advice {
+    
+    
+    public partial class Advice {
+    }
+}
