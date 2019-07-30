fbe5fc7c17493924836dcf76da0d1847278b83f6
rodela.corp <rodela.corp@AGCORP-WS14>
2019-07-30 15:17:17 +0600

    asset leave mushak added

---
+++ UI/Asset/Report/Asset_CheckInOut_Report.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.Asset.Report
+{
+    public partial class Asset_CheckInOut_Report : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'http://report.akij.net/reports/report/Asset_Module/Asset_CheckInOut_Report?rs:Embed=true');", true);
+
+        }
+    }
+}
---
+++ UI/HR/Reports/Employee_Leave_Movement_Absent_Report.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.ClassFiles;
+
+namespace UI.HR.Reports
+{
+    public partial class Employee_Leave_Movement_Absent_Report : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                pnlUpperControl.DataBind();
+            }
+            ScriptManager.RegisterStartupScript(Page, typeof(Page), "StartupScript", "loadIframe('frame', 'http://report.akij.net/reports/report/Common_Reports/Employee_Leave_Movement_Absent_Report?rs:Embed=true');", true);
+
+
+        }
+    }
+}
