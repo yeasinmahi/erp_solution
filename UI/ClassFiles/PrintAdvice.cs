6950a87975e7c33cf27e0b1bedeb342de4ee11cc
said <aid@AGCORP-WS17.com>
2019-08-06 15:57:06 +0600

    Print Advice

--- UI/ClassFiles/PrintAdvice.cs
+++ UI/ClassFiles/PrintAdvice.cs
@@ -42,6 +42,7 @@ namespace UI.ClassFiles
             BankVoucherPrintTDS.SprAccountsVoucherBankGetPrintVoucherDataDataTable itemTable = bnkv.GetBankVoucherPrintData(voucherID, userID,
                                                                                                                            ref bankName, ref accountNumber,
                                                                                                                            ref ChequeNumber, ref ChequeDate,
+                                                                                                                           
                                                                                                                            ref voucherNumber, ref voucherNarration,
                                                                                                                            ref totalAmount, ref unitName,
                                                                                                                            ref unitAddress, ref securityCode, ref vDate, ref unitID, ref chequeString);
@@ -55,28 +56,43 @@ namespace UI.ClassFiles
                 vitem[i].AccountCode = itemTable[i].strCode;
                 vitem[i].AccountName = itemTable[i].strAccName;
                 vitem[i].Description = itemTable[i].strNarration;
+                string strCode= itemTable[i].strCode;
+                string strAccName = itemTable[i].strAccName;
+                string strNarration = itemTable[i].strNarration;
+                decimal Amount = 0;
                 if (itemTable[i].monAmountControl == 0) // not Control Head
                 {
                     vitem[i].Amount = double.Parse("" + itemTable[i].monAmountSub);
                     vitem[i].YsnControlHead = false;
+
+                    Amount = itemTable[i].monAmountSub;
                 }
                 else
                 {
                     vitem[i].Amount = double.Parse("" + itemTable[i].monAmountControl);
                     vitem[i].YsnControlHead = true;
+
+                    Amount = itemTable[i].monAmountControl;
                 }
+                AmountFormat formatAmount = new AmountFormat();
+                totalAmountInWord = formatAmount.GetTakaInWords(totalAmount, "", "Only");
+                string[] addressLines = new string[1];
+                addressLines[0] = unitAddress;
+
+
+                    //InsertForPrint(voucherID, voucherDate, strForBarCode, strCode, strAccName, strNarration, Amount,
+                    //bankName, accountNumber, ChequeNumber, ChequeDate, voucherNumber, voucherNarration, 
+                    //totalAmount, unitName, unitAddress, securityCode, vDate, unitID, chequeString, totalAmountInWord);
+
 
             }
+          
+            
 
-            AmountFormat formatAmount = new AmountFormat();
-            totalAmountInWord = formatAmount.GetTakaInWords(totalAmount, "", "Only");
-            string[] addressLines = new string[1];
-            addressLines[0] = unitAddress;
-
-            htmlString = PreparePrintableBankVoucher(unitName, addressLines, voucherType, voucherNumber,
-                                                    voucherDate, vitem, totalAmountInWord, voucherNarration,
-                                                    bankName, accountNumber,
-                                                    ChequeNumber, CommonClass.GetShortDateAtLocalDateFormat(ChequeDate), chequeString);
+            //htmlString = PreparePrintableBankVoucher(unitName, addressLines, voucherType, voucherNumber,
+            //                                        voucherDate, vitem, totalAmountInWord, voucherNarration,
+            //                                        bankName, accountNumber,
+            //                                        ChequeNumber, CommonClass.GetShortDateAtLocalDateFormat(ChequeDate), chequeString);
 
 
             return htmlString;
